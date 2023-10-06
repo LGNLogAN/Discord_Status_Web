564718122e7c9cbbbcad562abedcb501e7d65082
LoGaN <80800012+LGNLogAN@users.noreply.github.com>
2023-10-06 17:35:31 +0900

    Update weatherAPI.js

--- Project_2/weatherAPI.js
+++ Project_2/weatherAPI.js
@@ -13,6 +13,9 @@ fetch(SeoulWeatherAPI).then(
     // console.log(json.main.temp);
 })
 
+// API를 통해 json 형식의 데이터를 받고 res 라는 매개변수에 저장하는데 res.json() 이 코드에서 json()이 함수를 통해 res 를 json
+// 형식으로 변환해서 res 매개변수에 API 를 통해 받은 json형식의 파일을 저장한다.
+// 그리고 나서 json1 이라는 변수는 res.json() 함수로 인해 반환된 json 형식의 데이터를 담고 있는것
 
 
 
