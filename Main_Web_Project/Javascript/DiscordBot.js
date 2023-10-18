5f25b76928c40a714a27d63dda7ab76e13b5f8c6
LoGaN <80800012+LGNLogAN@users.noreply.github.com>
2023-10-18 18:04:27 +0900

    a
    a

--- Main_Web_Project/Javascript/DiscordBot.js
+++ Main_Web_Project/Javascript/DiscordBot.js
@@ -3,17 +3,12 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('../JSON_File/DiscordAPIKey.json');
 const fs = require('fs');
 const filePath = './Main_Web_Project/JSON_File/channelInfo.json';
-const FirstVoiceChannel = '697642680292868112';
-const SecondVoiceChannel = '714629856691748895';
-const ThirdVoiceChannel = '842404940416679966';
-const FourthVoiceChannel = '842405026265301012';
 const express = require('express');
 const app = express();
 const port = 3594
 const date = new Date();
 var cors = require("cors");
-
-
+const voiceChannel = ['697642680292868112','714629856691748895','842404940416679966','842405026265301012'];
 function getDate(){
     var todayDate = `${ date.getFullYear() }년 ${ date.getMonth() + 1 }월 ${ date.getDate() }일`
     return todayDate;
@@ -25,49 +20,42 @@ function getTime() {
     let Nowtime = `${hours}시 ${minutes}분 ${seconds}초`;
     return Nowtime;
 }
-
-function update(){
+function update() {
+    const channelInfoArray = [];
     const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
-        client.once(Events.ClientReady, () => {
-            const date = new Date();
-            console.log(`만남의 광장의 서버데이터를 불러왔습니다.\n활성화 봇 : ${client.user.tag}\n요청 일자 >\n`,getDate(),'\n',getTime());
-            const guild = client.guilds.cache.first();
-            if (guild) {
-                const channel = guild.channels.cache.get(FirstVoiceChannel);
-                if (channel) {
-                    if (channel.type === 2) { 
-                        const channelInfo = {
-                            id: channel.id,
-                            name: channel.name,
-                            type: channel.type,
-                            guildId: guild.id,
-                            guildName: guild.name,
-                            guildIcon: guild.iconURL(),
-                            members: channel.members.map(member => ({
-                                id: member.id,
-                                username: member.user.username,
-                                discriminator: member.user.discriminator,
-                                avatar: member.user.avatarURL(),
-                                nickname: member.nickname
-                            }))
-                        };
-                        var channelInfoJson = JSON.stringify(channelInfo, null, 2);
-                        console.log('Discord Information :\n', channelInfoJson);
-                        fs.writeFileSync(filePath, channelInfoJson); 
-                    } else {
-                        console.error('이 채널은 음성채팅방이 아닙니다');
-                    }
-                } else {
-                    console.error('채널을 찾을 수 없습니다.');
-                }
-            } else {
-                console.error('서버를 찾을 수 없습니다.');
+    client.once(Events.ClientReady, () => {
+        console.log(`만남의 광장의 서버데이터를 불러왔습니다.\n활성화 봇 : ${client.user.tag}\n요청 일자 >\n`, getDate(), '\n', getTime());
+        const guild = client.guilds.cache.first();
+        if (guild) {
+            for (let i = 0; i < voiceChannel.length; i++) {
+                const channel = guild.channels.cache.get(voiceChannel[i]);
+                const channelInfo = {
+                    id: channel.id,
+                    name: channel.name,
+                    type: channel.type,
+                    guildId: guild.id,
+                    guildName: guild.name,
+                    guildIcon: guild.iconURL(),
+                    members: channel.members.map(member => ({
+                        id: member.id,
+                        username: member.user.username,
+                        discriminator: member.user.discriminator,
+                        avatar: member.user.avatar ? member.user.avatarURL() : 'https://github.com/LGNLogAN/Web/blob/main/Main_Web_Project/img/DIscordDefault.jpg?raw=true',
+                        nickname: member.nickname
+                    }))
+                };
+                channelInfoArray.push(channelInfo);
             }
-        });
-        client.login(token);
+            const channelInfoJson = JSON.stringify(channelInfoArray, null, 2);
+            console.log('Discord Information :\n', channelInfoJson);
+            fs.writeFileSync(filePath, channelInfoJson);
+        } else {
+            console.error("디스코드 서버가 존재하지 않습니다");
+        }
+    });
+
+    client.login(token);
 }
-
-
 app.use(cors());
 
 app.get('/discordAPI',(req , res) => {
@@ -86,4 +74,3 @@ app.get('/discordAPI',(req , res) => {
 app.listen(port , () => {
     console.log("데이터를 수신할 준비가 완료되었습니다.");
 })
-
--- Main_Web_Project/Javascript/DiscordMain.js
+++ Main_Web_Project/Javascript/DiscordMain.js
@@ -1,24 +1,68 @@
 function aa() {
+    const voiceChannel = ['697642680292868112','714629856691748895','842404940416679966','842405026265301012'];
     fetch('http://localhost:3594/discordAPI')
         .then(response => response.json())
         .then(json => {
-            document.querySelector(".DiscordServerIMG").src = json.guildIcon;
-            for (i = 0; i < json.members.length; i++){
+        document.querySelector(".DiscordServerIMG").src = json[0].guildIcon;
+        for (i = 0; i < json[0].members.length; i++){
+            const newTotalDiv = document.createElement('div');
+            document.querySelector(".voiceChannel_1_members").appendChild(newTotalDiv)
+            // .members 곳에 div 라는 자식 태그를 만들어줌
+            const newImg = document.createElement('img');
+            newTotalDiv.appendChild(newImg)
+            newImg.src = json[0].members[i].avatar;
+            // 그 자식태그 안에 img 라는 태그를 넣어줌
+            const newDiv = document.createElement('div');
+            newTotalDiv.appendChild(newDiv);
+            newDiv.append(json[0].members[i].username);
+            // 그 자식태그 안에 div(디스코드 닉네임)을 넣어줌
+        }
+        for (i = 0; i < json[1].members.length; i++){
+            const newTotalDiv = document.createElement('div');
+            document.querySelector(".voiceChannel_2_members").appendChild(newTotalDiv)
+            // .members 곳에 div 라는 자식 태그를 만들어줌
+            const newImg = document.createElement('img');
+            newTotalDiv.appendChild(newImg)
+            newImg.src = json[1].members[i].avatar;
+            // 그 자식태그 안에 img 라는 태그를 넣어줌
+            const newDiv = document.createElement('div');
+            newTotalDiv.appendChild(newDiv);
+            newDiv.append(json[1].members[i].username);
+            // 그 자식태그 안에 div(디스코드 닉네임)을 넣어줌
+        }
+        for (i = 0; i < json[2].members.length; i++){
+            const newTotalDiv = document.createElement('div');
+            document.querySelector(".voiceChannel_3_members").appendChild(newTotalDiv)
+            // .members 곳에 div 라는 자식 태그를 만들어줌
+            const newImg = document.createElement('img');
+            newTotalDiv.appendChild(newImg)
+            newImg.src = json[2].members[i].avatar;
+            // 그 자식태그 안에 img 라는 태그를 넣어줌
+            const newDiv = document.createElement('div');
+            newTotalDiv.appendChild(newDiv);
+            newDiv.append(json[2].members[i].username);
+            // 그 자식태그 안에 div(디스코드 닉네임)을 넣어줌
+        }
+        for (i = 0; i < json[3].members.length; i++){
+            const newTotalDiv = document.createElement('div');
+            document.querySelector(".voiceChannel_4_members").appendChild(newTotalDiv)
+            // .members 곳에 div 라는 자식 태그를 만들어줌
+            const newImg = document.createElement('img');
+            newTotalDiv.appendChild(newImg)
+            newImg.src = json[3].members[i].avatar;
+            // 그 자식태그 안에 img 라는 태그를 넣어줌
+            const newDiv = document.createElement('div');
+            newTotalDiv.appendChild(newDiv);
+            newDiv.append(json[3].members[i].username);
+            // 그 자식태그 안에 div(디스코드 닉네임)을 넣어줌
+        }
+        
+        
 
-                const newTotalDiv = document.createElement('div');
-                document.querySelector(".members").appendChild(newTotalDiv)
-                // .members 곳에 div 라는 자식 태그를 만들어줌
-                const newImg = document.createElement('img');
-                newTotalDiv.appendChild(newImg)
-                newImg.src = json.members[i].avatar;
-                // 그 자식태그 안에 img 라는 태그를 넣어줌
-                const newDiv = document.createElement('div');
-                newTotalDiv.appendChild(newDiv);
-                newDiv.append(json.members[i].username);
-                // 그 자식태그 안에 div(디스코드 닉네임)을 넣어줌
 
-            }
-        });
+
+
+
+    }); 
 }
-
 aa();
